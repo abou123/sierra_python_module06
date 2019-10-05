92526dd2686ef6db50d82068b940572bd24c1a50
abou123 <48367949+abou123@users.noreply.github.com>
2019-10-04 19:54:54 -0700

    Fixed problem with sample number

--- 2_formatting.py
+++ 2_formatting.py
@@ -16,7 +16,7 @@
 # Note that if only a single conversion specifier is used, a lone value can be supplied. If multiple conversion specifiers are present in the string,
 # then the values should be placed in a tuple, as in 'age of the %s is %d' % ('cat', user_age).
 
-# A conversion specifier includes a character (like the d in %d above), known as the conversion type, to indicate how to convert the value into the string. 
+# A conversion specifier includes a character (like the d in %d above), known as the conversion type, to indicate how to convert the value into the string.
 # The d means integer (d stands for decimal integer), whereas an f stands for float. Ex: '%f' % 5.2 yields the string '5.200000', while '%d' % 5.2 yields
 # the string '5' because the float is truncated when converted to an integer. Common conversion specifiers are integer (%d), floating-point (%f), and string (%s).
 
@@ -38,19 +38,19 @@
 
 # Student name (  Bob)
 
-# The output contains an additional two spaces prior to 'Bob' because the minimum field width is five and Bob is only three characters 
+# The output contains an additional two spaces prior to 'Bob' because the minimum field width is five and Bob is only three characters
 # (5 - 3 is 2).
 
 # An additional component known as conversion flags alter the output of conversion specifiers.
 # If the '0' conversion flag is included, numeric conversion types (%d, %f) add the leading 0s prescribed by the minimum field
-# width in place of spaces. Other conversion flags exist, such as '-', which left-justifies the formatted string, adding padding 
+# width in place of spaces. Other conversion flags exist, such as '-', which left-justifies the formatted string, adding padding
 # characters to the right instead of the left. Multiple conversion flags may be included in the same conversion specifier.
 # The conversion flags are always placed before the minimum field width:
 
 student_id = int(input('Enter student ID: '))
 
-print('The user entered %d' % student_id)
-print('Full 8-character student ID: %08d' % student_id)1234
+print('The user entered %b' % student_id)
+print('Full 8-character student ID: %08d' % student_id)
 
 # You might want to set how many digits to the right of a float-point decimal number to print.
 # The optional precision component of a conversion specifier indicates how many digits to the right of the decimal should be included.
